8581a3d1a7299f977110073f47018d38c4bb9f3d
pavel <pavel>
2016-11-03 22:50:56 +0200

    Initial Commit

---
+++ HappySwiftInstaClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HappySwiftInstaClient
+//
+//  Created by pavel on 11/3/16.
+//  Copyright © 2016 pavel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
