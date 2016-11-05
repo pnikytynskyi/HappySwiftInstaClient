bf28d22a886e51487661b840b9c0d7e4d1cc5402
pavel <pavel>
2016-11-05 13:40:20 +0200

    log in screen

--- HappySwiftInstaClient/ViewController.swift
+++ HappySwiftInstaClient/ViewController.swift
@@ -7,17 +7,45 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+//        self.borderColor()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    func borderColor() {
+        usernameTextField.layer.borderWidth = 1.0
+        passwordTextField.layer.borderWidth = 1.0
+    }
+    @IBAction func signInTapped(_ sender: AnyObject) {
+        let username = usernameTextField.text
+        let password = passwordTextField.text
+        FIRAuth.auth()?.signIn(withEmail: username!, password: password!, completion: { (user, error ) in
+            if error != nil {
+                let alert = UIAlertController(title: "Error", message: "Incorrect Username/Password", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                // error while logging in
+            } else {
+                let alert = UIAlertController(title: "Success", message: "You are logged in.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                // success
+            }
+        })
     }
 
 
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+use_frameworks!
+target 'HappySwiftInstaClient' do
+ 
+pod 'Firebase/Core'
+pod 'Firebase/Auth'
+
+
+  target 'HappySwiftInstaClientTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
